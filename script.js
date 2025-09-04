d0dcf4472107eaaa7c4feb7651212f78d541a95a
Rayhhh007 <00001107539791sp@al.educacao.sp.gov.br>
2025-09-04 14:24:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
                           aumentaFonteBotao.addEventListener('click', function () {
  tamanhoAtualFonte += 0.1;
  document.body.style.fontSize = \${tamanhoAtualFonte}rem``
-11
+
  })
-13
-14 diminuiFonteBotao.addEventListener('click', function () {
-15 tamanhoAtualFonte -= 0.1;
-16 document.body.style.fontSize = \${tamanhoAtualFonte}rem``
-17
-18 })
-19
-20 })
+
+ diminuiFonteBotao.addEventListener('click', function () {
+ tamanhoAtualFonte -= 0.1;
+ document.body.style.fontSize = \${tamanhoAtualFonte}rem``
+
+ })
+
+ })
